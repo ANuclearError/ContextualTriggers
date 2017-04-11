a7528d3e64b36939a725241692ca88fc7acc83d3
Aidan O'Grady <aidan@aidanogrady.com>
2017-04-11 13:02:54 +0100

    Renamed Places to PlacesDataSource.

--- app/src/main/java/com/aidanogrady/contextualtriggers/context/data/PlacesDataSource.java
+++ app/src/main/java/com/aidanogrady/contextualtriggers/context/data/PlacesDataSource.java
@@ -5,15 +5,18 @@ import android.location.Location;
 import com.aidanogrady.contextualtriggers.context.ListenerContext;
 import com.google.android.gms.location.LocationListener;
 
-/**
- * Created by thomas on 10/04/17.
- */
 
-public class Places extends ListenerContext implements LocationListener {
+/**
+ * The places data source utilises Google's Place API Web Service to obtain interesting locations
+ * near the user.
+ *
+ * @author Aidan O'Grady
+ * @since 0.1
+ */
+public class PlacesDataSource extends ListenerContext implements LocationListener {
 
     @Override
     public void onLocationChanged(Location location) {
 
     }
-
 }
