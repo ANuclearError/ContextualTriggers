ddbede84e8a7787a4a79eb90e2467cdbff04e015
Calum McGarry <clb12162@uni.strath.ac.uk>
2017-04-12 20:02:35 +0100

    Add class to get weather data asynchronously

--- app/src/main/java/com/aidanogrady/contextualtriggers/MainActivity.java
+++ app/src/main/java/com/aidanogrady/contextualtriggers/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.aidanogrady.contextualtriggers.context.data.LocationDataSource;
+import com.aidanogrady.contextualtriggers.context.data.WeatherDataSource;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
 
+        WeatherDataSource wsd = new WeatherDataSource();
+        wsd.execute();
         Intent intent = new Intent(this, LocationDataSource.class);
         startService(intent);
         finish();
---
+++ app/src/main/java/com/aidanogrady/contextualtriggers/context/data/WeatherDataSource.java
@@ -0,0 +1,62 @@
+package com.aidanogrady.contextualtriggers.context.data;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Calum on 12/04/2017.
+ */
+
+public class WeatherDataSource extends AsyncTask{
+
+    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?";
+    private static String PARAMS = "q=Glasgow,UK&APPID=72fea7c50a5622a959485e2731ce1f71";
+
+    HttpURLConnection weatherConnection = null;
+    InputStream stream = null;
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        try {
+            weatherConnection = (HttpURLConnection) (new URL(BASE_URL + PARAMS)).openConnection();
+            weatherConnection.setRequestMethod("GET");
+            weatherConnection.setDoInput(true);
+            weatherConnection.setDoOutput(true);
+            weatherConnection.connect();
+
+
+            StringBuilder buffer = new StringBuilder();
+            stream = weatherConnection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+            String line;
+            while ( (line = br.readLine()) != null )
+                buffer.append(line).append("rn");
+
+            stream.close();
+            weatherConnection.disconnect();
+
+            JSONObject jsonObject = new JSONObject(buffer.toString());
+            JSONArray jsonArray = jsonObject.getJSONArray("list");
+            String dateval = (String) jsonArray.getJSONObject(0).get("dt_txt");
+
+            System.out.println(dateval);
+
+            return null;
+
+        }catch(Exception e){
+            System.out.println("An exception happened: " + e);
+            return null;
+        }
+
+    }
+
+
+}
